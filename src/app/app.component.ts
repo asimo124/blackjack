599c3bfe650b8bbe41cea9fafb461b6a58737c3f
alex hawley <alex@engage2learn.org>
2021-05-17 21:08:45 -0500

    - Finished project except for more testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,23 @@ export class AppComponent implements OnInit {
   public dealCount = 0;
   public runningCount = 0;
   public guessedRunningCount = 0;
+  public lastGuessedCount = 0;
   public currentCount = null;
   public invalidCurrentCount = false;
 
   public hitStack: any[] = [];
+  public playedRounds = [];
+  public currentRound = [];
+  public currentRoundIndex = 0;
+  public roundsGuessedCount = [];
+  public roundsGuessedRunningCount = [];
+  public roundsCorrectCount = [];
+  public roundsCorrectRunningCount = [];
 
   public logCount = 0;
+
+  isReplayingDeck = false;
+  canShowReplay = false;
 
   constructor(private blackJackService: BlackJackService) {
 
@@ -41,16 +52,19 @@ export class AppComponent implements OnInit {
     if (doShuffle) {
       this.deck = this.blackJackService.shuffleDeck();
     }
+    this.playedRounds = [];
+    this.canShowReplay = false;
+    this.isReplayingDeck = false;
 
     switch (this.numPlayers) {
       case 2:
-        this.playerSpan = 6;
-        break;
-      case 3:
         this.playerSpan = 8;
         break;
-      case 4:
+      case 3:
         this.playerSpan = 6;
+        break;
+      case 4:
+        this.playerSpan = 4;
         break;
     }
 
@@ -59,6 +73,7 @@ export class AppComponent implements OnInit {
     this.guessedRunningCount = 0;
     this.hitStack = [];
 
+    this.currentRoundIndex = 0;
     this.currentDeckIndex = 0;
     this.dealNextHand();
   }
@@ -72,18 +87,20 @@ export class AppComponent implements OnInit {
   }
 
   validateCurrentCount() {
-    if (this.currentCount === null) {
+    if (this.lastGuessedCount === null) {
       return false;
     }
-    if (isNaN(this.currentCount)) {
+    if (isNaN(this.lastGuessedCount)) {
       return false;
     }
+    const lastGuessedCount = parseInt(String(this.lastGuessedCount), 10);
+    this.lastGuessedCount = lastGuessedCount;
     return true;
   }
 
   setCountAndDeal() {
 
-    /*/
+    //*/
     this.invalidCurrentCount = false;
     if (!this.validateCurrentCount()) {
       this.invalidCurrentCount = true;
@@ -94,18 +111,64 @@ export class AppComponent implements OnInit {
   }
 
   canDealNextHand() {
-    if (this.currentDeckIndex < 52) {
+
+    // return false;
+
+    // console.log('this.currentDeckIndex: ', this.currentDeckIndex);
+
+    if (this.currentDeckIndex < 51) {
       return true;
     }
     return false;
   }
 
+  canShowReplayButton() {
+
+    console.log('this.playedRounds.lenghtn: ', this.playedRounds.length);
+    console.log('this.roundsGuessedCount.lenghtn: ', this.roundsGuessedCount.length);
+    console.log('this.currentRoundIndex: ', this.currentRoundIndex);
+    console.log('this.canDealNextHand: ', this.canDealNextHand());
+    if (!this.canDealNextHand() && this.playedRounds.length === this.roundsGuessedCount.length) {
+      this.canShowReplay = true;
+    } else {
+      this.canShowReplay = false;
+    }
+    console.log('this.canShowReplay: ', this.canShowReplay);
+  }
+
   dealNextHand() {
 
+    console.log('this.currentRoundIndex: ', this.currentRoundIndex);
+
+    let didPushRound = false;
     if (!this.canDealNextHand()) {
+      didPushRound = true;
+      this.playedRounds.push(this.currentRound);
+    }
+    if (this.currentRound.length > 0) {
+      if (!didPushRound) {
+        this.playedRounds.push(this.currentRound);
+      }
+
+      this.roundsGuessedCount.push(this.lastGuessedCount);
+      this.guessedRunningCount += this.lastGuessedCount;
+      this.roundsGuessedRunningCount.push(this.guessedRunningCount);
+
+      this.roundsCorrectCount.push(this.dealCount);
+      this.roundsCorrectRunningCount.push(this.runningCount);
+
+      this.lastGuessedCount = null;
+    }
+    if (!this.canDealNextHand()) {
+      this.canShowReplayButton();
       return;
+    } else {
+      this.currentRoundIndex++;
     }
 
+
+    this.currentRound = [];
+    this.hitStack = [];
     let eachPlayerHand = [];
     let nextCard = null;
     let eachValue = null;
@@ -121,7 +184,10 @@ export class AppComponent implements OnInit {
           eachValue = this.blackJackService.getCardValue(nextCard.value);
           this.dealCount += eachValue;
           eachPlayerHand.push(nextCard);
+          this.currentRound.push(nextCard);
           this.currentDeckIndex++;
+        } else {
+          // this.playedRounds.push(this.currentRound);
         }
       }
       this.playerHands.push(eachPlayerHand);
@@ -133,20 +199,22 @@ export class AppComponent implements OnInit {
       eachValue = this.blackJackService.getCardValue(nextCard.value);
       this.dealCount += eachValue;
       eachPlayerHand.push(nextCard);
+      this.currentRound.push(nextCard);
       this.currentDeckIndex++;
+    } else {
+      // this.playedRounds.push(this.currentRound);
     }
 
     this.playerHands.push(eachPlayerHand);
 
     this.runningCount += this.dealCount;
-    this.currentCount = null;
 
     console.log('runningCount ' + this.logCount + ': ', this.runningCount);
   }
 
   changedPlayersNum() {
     this.deck = this.blackJackService.shuffleDeck();
-    this.startGame();
+    this.startGame(true);
   }
 
   hitPlayer() {
@@ -155,13 +223,57 @@ export class AppComponent implements OnInit {
       if (this.canDealNextHand()) {
         const nextCard = this.deck[this.currentDeckIndex];
         this.hitStack.push(nextCard);
+        this.currentRound.push(nextCard);
 
         const eachValue = this.blackJackService.getCardValue(nextCard.value);
         this.dealCount += eachValue;
-        this.runningCount += this.dealCount;
+        this.runningCount += eachValue;
 
         this.currentDeckIndex++;
+      } else {
+        // this.playedRounds.push(this.currentRound);
       }
     }
   }
+
+  replayDeck() {
+    this.isReplayingDeck = true;
+  }
+
+  movePrevious() {
+    if (this.currentRoundIndex > 1) {
+      this.currentRoundIndex--;
+    }
+  }
+
+  moveNext() {
+    if (this.currentRoundIndex < this.playedRounds.length - 1) {
+      this.currentRoundIndex++;
+    }
+  }
+
+  moveFirst() {
+    this.currentRoundIndex = 1;
+  }
+
+  moveLast() {
+    this.currentRoundIndex = this.playedRounds.length - 1;
+  }
+
+  getRoundStyles() {
+    if (this.roundsGuessedCount[this.currentRoundIndex] === this.roundsCorrectCount[this.currentRoundIndex]) {
+      return '#98fab8';
+    } else {
+      return '#fcb7a9';
+    }
+  }
+
+  getCountStyles() {
+    if (this.roundsGuessedRunningCount[this.currentRoundIndex] === this.roundsCorrectRunningCount[this.currentRoundIndex]) {
+      return '#98fab8';
+    } else {
+      return '#fcb7a9';
+    }
+  }
+
 }
